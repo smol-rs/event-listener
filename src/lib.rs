1587b7299d4a20916a703cb5ff9fcb074839079b
jtnunley <jtnunley01@gmail.com>
2022-09-09 09:54:47 -0700

    fix MIRI failure

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,9 @@
 //!
 //!     // Wait for a notification and continue the loop.
 //!     listener.wait();
-//! }
+//! } 
+//! # // Sleep to prevent MIRI failure, https://github.com/rust-lang/miri/issues/1371
+//! # std::thread::sleep(std::time::Duration::from_secs(3));
 //! ```
 
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
