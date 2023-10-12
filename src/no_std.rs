af4bbd46970e03ded71d606411a1b85ddb6c799d
scirin <irine.kokilashvili@gmail.com>
2023-10-12 21:22:44 +0400

    [FIX] fix fmt

--- src/no_std.rs
+++ src/no_std.rs
@@ -242,11 +242,11 @@ impl<T> List<T> {
     }
     pub fn total_listeners(&self) -> usize {
         self.inner
-        .try_lock()
-        .as_ref()
-        .map(|lock| &**lock)
-        .map(|listener_slab| listener_slab.listeners.len())
-        .unwrap_or(0)
+            .try_lock()
+            .as_ref()
+            .map(|lock| &**lock)
+            .map(|listener_slab| listener_slab.listeners.len())
+            .unwrap_or(0)
     }
 }
 
