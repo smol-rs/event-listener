70da197003f5b66fd4e33a62e53b3d8795ad3c6a
John Nunley <dev@notgull.net>
2024-04-13 20:48:29 -0700

    m: Fix MSRV
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -1,11 +1,11 @@
 //! Linked list of listeners.
 
 #[cfg(feature = "std")]
-mod std;
+mod mutex;
 #[cfg(feature = "std")]
-pub(crate) use std::*;
+pub(crate) use mutex::*;
 
 #[cfg(not(feature = "std"))]
-mod no_std;
+mod lock_free;
 #[cfg(not(feature = "std"))]
-pub(crate) use no_std::*;
+pub(crate) use lock_free::*;
