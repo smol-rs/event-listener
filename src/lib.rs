da7b6f8b56f1311b57a4d57dff126f692ce6deea
John Nunley <dev@notgull.net>
2024-04-13 21:04:06 -0700

    docs: Add smol-rs logo
    cc #63
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,12 @@
 //!
 //! [`portable-atomic`]: https://crates.io/crates/portable-atomic
 
+#![doc(
+    html_favicon_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
 #![no_std]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 
