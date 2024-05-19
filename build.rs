cd159e6a1ca2a161625a5f5419ce191fe8b77ec1
John Nunley <dev@notgull.net>
2024-05-19 15:23:36 -0700

    m: Use Cargo.toml lint fix instead of build.rs
    https://github.com/smol-rs/event-listener/pull/135#discussion_r1606080083
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- build.rs
+++
@@ -1,7 +0,0 @@
-//! Emits the necessary "checked" compile time flag warning for "loom".
-//!
-//! Without this, the compiler complains about the unspecified "loom" flag.
-
-fn main() {
-    println!("cargo:rustc-check-cfg=cfg(loom)");
-}
